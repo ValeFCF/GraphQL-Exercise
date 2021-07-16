87fa36265d9cd95ec33fd37ebd0c6737e24dbe09
ValeFCF <valefcfhc@gmail.com>
2021-07-16 14:09:50 -0500

    first commit

---
+++ GraphQL-Exercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GraphQL-Exercise
+//
+//  Created by Valentín Granados on 16/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
